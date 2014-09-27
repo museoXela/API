66a66b69210c57dbcd9346c22e76d1e287db1b0d
Estuardo Díaz <esjoando555@gmail.com>
2014-09-27 14:08:59 -0600

    ACTUALIZACION - countries, usuarios
    Se crearon modelos de perfil de usuario y paises

---
+++ countries/admin.py
@@ -0,0 +1,10 @@
+# To change this template, choose Tools | Templates
+# and open the template in the editor.
+
+__author__="t4r0"
+__date__ ="$16-oct-2013 18:46:32$"
+
+from django.contrib import admin
+from countries.models import Country
+
+admin.site.register(Country)
