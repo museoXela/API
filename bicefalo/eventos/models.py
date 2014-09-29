9e5c2f43853e8f68c4ae1416e5d0ed904a720071
Victor Betancohurt <victorbetha@gmail.com>
2014-09-29 00:20:35 -0600

    Revert "ACTUALIZACION MODELS"
    This reverts commit 7ea8eaa42603f3b44df31d723a602ad6a39697bc.
    
    Conflicts:
    	bicefalo/eventos/admin.py
    	bicefalo/eventos/models.py
    	bicefalo/eventos/resources.py
    	bicefalo/eventos/tests.py

--- bicefalo/eventos/models.py
+++ bicefalo/eventos/models.py
@@ -2,3 +2,9 @@ from django.db import models
 #esto es para agregar eso from usuario.models import Perfil
 # Create your models here.
 class Eventos (models.Model):
+    nombre = models.TextField(null=True,blank=True)
+    descripcion=models.TextField(null=True,blank=True)
+    aficeh =models.TextField(null=True,blank=True)
+    fecha=models.DateField(auto_now=True, blank=True, null=True)
+    sala=models.ForeignKey('traslados.models.Sala')
+    usuario =models.ForeignKey('usuarios.models.usuario')
