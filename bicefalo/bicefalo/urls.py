980b6e68dbf94c3d963503d98a76ebc4bae1f18b
Estuardo Díaz <esjoando555@gmail.com>
2014-10-29 21:15:52 -0600

    Cambios en las url

--- bicefalo/bicefalo/urls.py
+++ bicefalo/bicefalo/urls.py
@@ -20,9 +20,9 @@ autodiscover('resources','enabled_resources', register_resources)
 autodiscover('resources','web_resources', register_web_resources)
 
 urlpatterns = patterns('',
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^oAuth/', include('provider.oauth2.urls')),
-    url(r'^web/', include(web_api.urls)),
-    url(r'^api/', include(api.urls)),
+    url(r'/admin/', include(admin.site.urls)),
+    url(r'/oAuth/', include('provider.oauth2.urls')),
+    url(r'/web/', include(web_api.urls)),
+    url(r'/api/', include(api.urls)),
 )
 
