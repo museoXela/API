2bb3cd4ec48229f52e3992277fac3047e4941113
Estuardo Díaz <esjoando555@gmail.com>
2014-11-07 10:43:57 -0600

    ACTUALIZACION
    Arreglado el recurso de traslados

--- bicefalo/traslados/resources.py
+++ bicefalo/traslados/resources.py
@@ -53,18 +53,14 @@ class Traslado(CustomResource):
 
     def hydrate_vitrina(self, bundle):
         from tastypie import http
-        from django.db.models import exceptions
         vitrina = bundle.data['vitrina']
         if vitrina:
-            try:
-                vitrina = Vitrinas.objects.get(id=vitrina)
-                if vitrina:
-                    bundle.data['vitrina']=vitrina
-                    return bundle
-                else:
-                    raise http.HttpBadRequest('Traslado debe degfinir una caja o una vitrina')
-            except exceptions.ObjectDoesNotExist:
-                raise http.HttpNotFound('no existe una vitrina con ese número de registro')
+            vitrina = Vitrinas.objects.get(id=vitrina)
+            if vitrina:
+                bundle.data['vitrina']=vitrina
+                return bundle
+            else:
+                raise http.HttpBadRequest('Traslado debe degfinir una caja o una vitrina')
         return bundle
 
 
